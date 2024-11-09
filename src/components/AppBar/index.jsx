b2586148f3f72445582bed17c43fa0f5ba8e63f9
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2024-11-09 15:22:08 +0700

    [Lecture 22] Code for Trello AppBar - Theme Override (part 2)

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -31,7 +31,12 @@ const AppBar = () => {
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }} />
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-          <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'primary.main' }} />
+          <SvgIcon
+            component={TrelloIcon}
+            fontSize='small'
+            inheritViewBox
+            sx={{ color: 'primary.main' }}
+          />
           <Typography
             variant='span'
             sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}
@@ -51,14 +56,14 @@ const AppBar = () => {
         <TextField id='outlined-search' label='Search...' size='small' type='search' />
         <ModeSelect />
 
-        <Tooltip title='Notification'>
+        <Tooltip title='Notifications'>
           <Badge color='secondary' variant='dot' sx={{ cursor: 'pointer' }}>
-            <NotificationsNoneIcon />
+            <NotificationsNoneIcon sx={{ color: 'primary.main' }} />
           </Badge>
         </Tooltip>
 
         <Tooltip title='Help'>
-          <HelpOutlineIcon sx={{ cursor: 'pointer' }} />
+          <HelpOutlineIcon sx={{ cursor: 'pointer', color: 'primary.main' }} />
         </Tooltip>
         <Profiles />
       </Box>
