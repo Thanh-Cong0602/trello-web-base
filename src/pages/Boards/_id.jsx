1a79ae43daee5376a60d2e558747665321ab50a4
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2024-11-19 03:34:43 +0700

    [Lecture 61] Axios: Call API from Front-end side to Clean Code

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,15 +1,24 @@
 import Container from '@mui/material/Container'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from '~/pages/Boards/BoardBar/BoardBar'
 import BoardContent from '~/pages/Boards/BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
 
 const Board = () => {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    const boardId = '673b9adcf1c9a4e6b059b434'
+    fetchBoardDetailsAPI(boardId).then(board => {
+      setBoard(board)
+    })
+  }, [])
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
