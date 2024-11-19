d3e55251ebfdff0c80cfa7aac7660124ec43cbbd
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2024-11-19 16:32:42 +0700

    [Lecture 69] Merge 2 APIs to create Column & Card into Trello interface (part 2)

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,28 +1,49 @@
 import Container from '@mui/material/Container'
+import { isEmpty } from 'loadsh'
 import { useEffect, useState } from 'react'
 import { createNewCardAPI, createNewColumnAPI, fetchBoardDetailsAPI } from '~/apis'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from '~/pages/Boards/BoardBar/BoardBar'
 import BoardContent from '~/pages/Boards/BoardContent/BoardContent'
-
+import { generatePlaceholderCard } from '~/utils/formatters'
 const Board = () => {
   const [board, setBoard] = useState(null)
 
   useEffect(() => {
     const boardId = '673baa23124fb358072bb960'
+
     fetchBoardDetailsAPI(boardId).then(board => {
+      board.columns.forEach(column => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)]
+          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+        }
+      })
+
       setBoard(board)
     })
   }, [])
 
   const createNewColumn = async newColumnData => {
     const createdColumn = await createNewColumnAPI({ ...newColumnData, boardId: board._id })
-    console.log('createdColumn', createdColumn)
+
+    createdColumn.cards = [generatePlaceholderCard(createdColumn)]
+    createdColumn.cardOrderIds = [generatePlaceholderCard(createdColumn)._id]
+
+    const newBoard = { ...board }
+    newBoard.columns.push(createdColumn)
+    newBoard.columnOrderIds.push(createdColumn._id)
+    setBoard(newBoard)
   }
 
   const createNewCard = async newCardData => {
     const createdCard = await createNewCardAPI({ ...newCardData, boardId: board._id })
-    console.log('createdCard', createdCard)
+    const newBoard = { ...board }
+    const columnToUpdate = newBoard.columns.find(column => column._id === createdCard.columnId)
+    if (columnToUpdate) {
+      columnToUpdate.cards.push(createdCard)
+      columnToUpdate.cardOrderIds.push(createdCard._id)
+    }
   }
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
