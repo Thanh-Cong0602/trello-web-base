9436193c6459a4aa0d8ba6d8bec7eeaf540b35e1
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2024-11-20 18:31:52 +0700

    [Session 4] Session 4: React Router DOM & React Hook Form, Auth (Login Register) & 404 pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import NotFound from '~/pages/404/NotFound'
+import Auth from '~/pages/Auth/Auth'
 import Board from '~/pages/Boards/_id'
 
 function App() {
   return (
-    <>
-      <Board />
-    </>
+    <Routes>
+      <Route path='/' element={<Navigate to='boards/673baa23124fb358072bb960' replace={true} />} />
+
+      <Route path='/boards/:boardId' element={<Board />} />
+
+      {/* Authentication */}
+      <Route path='/login' element={<Auth />} />
+      <Route path='/register' element={<Auth />} />
+
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
